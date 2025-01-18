3c1bfa55d62a8cb2e0a509c119f4764478242ebc
Pascalpex <pascalpex@gmail.com>
2025-01-18 14:25:50 +0100

    Initial plugin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,34 @@
 plugins {
     id("java")
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
 }
 
 group = "de.pascalpex"
-version = "1.0-SNAPSHOT"
+version = "2.0"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
 }
 
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.assemble {
+    dependsOn(tasks.reobfJar)
+}
 tasks.test {
     useJUnitPlatform()
 }
+
+paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
---
+++ src/main/java/de/pascalpex/pexnpc/Main.java
@@ -0,0 +1,16 @@
+package de.pascalpex.pexnpc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
